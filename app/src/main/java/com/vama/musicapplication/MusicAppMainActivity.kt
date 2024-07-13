acb776f6c3046bcd901d10b44418fea40588cb0d
AsmaaMarazki <asmaa.marazki@gmail.com>
2024-07-13 18:24:05 +0300

    feature: splash screen

--- app/src/main/java/com/vama/musicapplication/MusicAppMainActivity.kt
+++ app/src/main/java/com/vama/musicapplication/MusicAppMainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.vama.musicapplication.navigation.VamaMusicNavHost
 import com.vama.musicapplication.ui.theme.MusicApplicationTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MusicAppMainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
